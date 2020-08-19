d121fe1021f909d7d6e7670ab5fa8f2fada22c3a
ArdGoossens <38463688+ArdGoossens@users.noreply.github.com>
2020-08-19 14:24:10 +0200

    Update: README.md note1.py

--- note1.py
+++ note1.py
@@ -29,3 +29,23 @@
 
 # COMMAND ----------
 
+# MAGIC %scala
+# MAGIC // import into cluster  com.microsoft.azure:azure-sqldb-spark:1.0.2.
+# MAGIC 
+# MAGIC import com.microsoft.azure.sqldb.spark.config.Config
+# MAGIC import com.microsoft.azure.sqldb.spark.connect._
+# MAGIC 
+# MAGIC val config = Config(Map(
+# MAGIC   "url"          -> "server00000s7qefz5aot56o.database.windows.net",
+# MAGIC   "databaseName" -> "database000s7qefz5aot56o",
+# MAGIC   "queryCustom"  -> "SELECT *  FROM [dbo].[Datamart]",  //Sql query
+# MAGIC   "user"         -> "Ard",
+# MAGIC   "password"     -> "Goossens.",
+# MAGIC ))
+# MAGIC 
+# MAGIC 
+# MAGIC val collection = sqlContext.read.sqlDB(config)
+# MAGIC collection.show()
+
+# COMMAND ----------
+
