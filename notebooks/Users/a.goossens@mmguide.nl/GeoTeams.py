49ad6c62f4e2a616b65576c055064b5e7e6ff3b0
ArdGoossens <38463688+ArdGoossens@users.noreply.github.com>
2020-08-25 16:19:24 +0200

    Update: note1.py notebooks

--- notebooks/Users/a.goossens@mmguide.nl/GeoTeams.py
+++ notebooks/Users/a.goossens@mmguide.nl/GeoTeams.py
@@ -1,12 +1,13 @@
 # Databricks notebook source
-# MAGIC %sh
-# MAGIC curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-# MAGIC curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list 
-# MAGIC apt-get update
-# MAGIC ACCEPT_EULA=Y apt-get install msodbcsql17
-# MAGIC apt-get -y install unixodbc-dev
-# MAGIC sudo apt-get install python3-pip -y
-# MAGIC pip3 install --upgrade pyodbc
+#https://datathirst.net/blog/2018/10/12/executing-sql-server-stored-procedures-on-databricks-pyspark
+%sh
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list 
+apt-get update
+ACCEPT_EULA=Y apt-get install msodbcsql17
+apt-get -y install unixodbc-dev
+sudo apt-get install python3-pip -y
+pip3 install --upgrade pyodbc
 
 # COMMAND ----------
 
