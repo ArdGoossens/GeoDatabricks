b779cfb2529400cfef4a0253a1945cebc15d510f
ArdGoossens <38463688+ArdGoossens@users.noreply.github.com>
2020-08-27 09:10:20 +0200

    Update: note1.py notebooks

--- notebooks/Users/a.goossens@mmguide.nl/GeoTeams.py
+++ notebooks/Users/a.goossens@mmguide.nl/GeoTeams.py
@@ -3,23 +3,6 @@
 
 # COMMAND ----------
 
-# MAGIC %sh
-# MAGIC curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-# MAGIC curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list 
-# MAGIC apt-get update
-# MAGIC ACCEPT_EULA=Y apt-get install msodbcsql17
-# MAGIC apt-get -y install unixodbc-dev
-# MAGIC sudo apt-get install python3-pip -y
-# MAGIC pip3 install --upgrade pyodbc
-
-# COMMAND ----------
-
-import re
-from pyspark.sql.functions import lit
-from pyspark.sql.functions import explode
-from pyspark.sql.functions import to_json, struct
-
-
 #filename ="DINOBRO_Entities_20200623.json"
 #filename ="DINOBRO_EntityDescriptions_20200623.json"
 #filename ="DINOBRO_TimeEntities_20200623.json"
@@ -27,8 +10,6 @@ from pyspark.sql.functions import to_json, struct
 #filename ="SUNFLOWER_EntityDescriptions_20200616.json"
 #filename ="SUNFLOWER_TimeEntities_20200616.json"
 
-
-#dbutils.notebook.run("/Users/a.goossens@mmguide.nl/GeoTeam", 60, {"file": "DINOBRO_EntityDescriptions_20200623.json"})
 
 
 # COMMAND ----------
@@ -54,19 +35,3 @@ from pyspark.sql.functions import to_json, struct
 # COMMAND ----------
 
 # MAGIC %run /Users/a.goossens@mmguide.nl/GeoTeam $file =SUNFLOWER_TimeEntities_20200616.json
-
-# COMMAND ----------
-
-#import pyodbc
-#conn = pyodbc.connect( 'DRIVER={ODBC Driver 17 for SQL Server};'
-#                       'SERVER=server00000nd4wods4xqefm.database.windows.net;'
-#                       'DATABASE=database000nd4wods4xqefm;UID=Ard;'
-#                       'PWD=Goossens.')
-#conn.autocommit = True
-
-#jdbc:sqlserver://server00000nd4wods4xqefm.database.windows.net:1433;database=database000nd4wods4xqefm;user=Ard@server00000nd4wods4xqefm;password=Goossens.;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;
-
-# Example doing a simple execute
-#conn.execute('exec team.stp_import')
-
-#conn.close()
