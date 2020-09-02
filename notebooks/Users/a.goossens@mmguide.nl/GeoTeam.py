c3c5621cad50bcfbf641d8af5234e95515ac990b
ArdGoossens <38463688+ArdGoossens@users.noreply.github.com>
2020-09-02 11:07:30 +0200

    Update: note1.py notebooks

--- notebooks/Users/a.goossens@mmguide.nl/GeoTeam.py
+++ notebooks/Users/a.goossens@mmguide.nl/GeoTeam.py
@@ -6,19 +6,21 @@ filename= dbutils.widgets.get("file")
 Filetype=""
 Customer=""
 
+
+
 import datetime
 nu= datetime.datetime.now()
 
 # COMMAND ----------
 
-#%sh
-#curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-#curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list 
-#apt-get update
-#ACCEPT_EULA=Y apt-get install msodbcsql17
-#apt-get -y install unixodbc-dev
-#sudo apt-get install python3-pip -y
-#pip3 install --upgrade pyodbc
+# MAGIC %sh
+# MAGIC curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+# MAGIC curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list 
+# MAGIC apt-get update
+# MAGIC ACCEPT_EULA=Y apt-get install msodbcsql17
+# MAGIC apt-get -y install unixodbc-dev
+# MAGIC sudo apt-get install python3-pip -y
+# MAGIC pip3 install --upgrade pyodbc
 
 # COMMAND ----------
 
@@ -234,7 +236,7 @@ if SUN6==1:
 # COMMAND ----------
 
 #DINO4DF.printSchema()
-display(DINO4DF)
+
 
 # COMMAND ----------
 
@@ -268,9 +270,9 @@ if SUN6==1:
 
 # COMMAND ----------
 
-#import pyodbc
-#conn = pyodbc.connect( odbc_ConnectionString)
-#conn.autocommit = True
-#command =  "exec team.stp_import '" + Filetype +"','"+str(nu)+"'" 
-#conn.execute(command)
-#conn.close()
+import pyodbc
+conn = pyodbc.connect( odbc_ConnectionString)
+conn.autocommit = True
+command =  "exec gen.stp_import '"+str(nu)+"'" 
+conn.execute(command)
+conn.close()
