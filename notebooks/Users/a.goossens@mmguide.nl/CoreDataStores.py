c4de10789e3f0fc49a6d6bb72a02ba2595be7d7f
ArdGoossens <38463688+ArdGoossens@users.noreply.github.com>
2020-09-02 08:39:23 +0200

    Update: note1.py notebooks

--- notebooks/Users/a.goossens@mmguide.nl/CoreDataStores.py
+++ notebooks/Users/a.goossens@mmguide.nl/CoreDataStores.py
@@ -1,1 +1,22 @@
 # Databricks notebook source
+# MAGIC %run /Users/a.goossens@mmguide.nl/CoreDataStore $file =DINOBRO_Entities_20200623.json
+
+# COMMAND ----------
+
+# MAGIC %run /Users/a.goossens@mmguide.nl/CoreDataStore $file =DINOBRO_EntityDescriptions_20200623.json
+
+# COMMAND ----------
+
+# MAGIC %run /Users/a.goossens@mmguide.nl/CoreDataStore $file =DINOBRO_TimeEntities_20200623.json
+
+# COMMAND ----------
+
+# MAGIC %run /Users/a.goossens@mmguide.nl/CoreDataStore $file =SUNFLOWER_Entities_20200616.json
+
+# COMMAND ----------
+
+# MAGIC %run /Users/a.goossens@mmguide.nl/CoreDataStore $file =SUNFLOWER_EntityDescriptions_20200616.json
+
+# COMMAND ----------
+
+# MAGIC %run /Users/a.goossens@mmguide.nl/CoreDataStore $file =SUNFLOWER_TimeEntities_20200616.json
